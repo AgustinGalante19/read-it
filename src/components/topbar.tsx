da4d4d61f1b2d46e349e376cd02d421470684b62
Agustin Galante <agustin.galante.19@outlook.es>
2024-11-29 21:41:14 -0300

    icon based on time desc

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -1,11 +1,21 @@
 import getTimeDescOfDay from '@/lib/getTimeDescOfDay';
+import { CloudSun, Moon, Sun } from 'lucide-react';
 import Image from 'next/image';
 
 function Topbar() {
+  const timeDesc = getTimeDescOfDay();
+
   return (
     <header className='py-4 w-full flex justify-between items-center container mx-auto px-6'>
-      <span className='text-2xl font-bold text-white'>
-        Good {getTimeDescOfDay()}
+      <span className='text-2xl font-bold text-white flex items-center gap-2'>
+        {timeDesc === 'Morning' ? (
+          <Sun className='text-primary' />
+        ) : timeDesc === 'Afternoon' ? (
+          <CloudSun className='text-primary' />
+        ) : (
+          <Moon className='text-primary' />
+        )}
+        <span>Good</span> {timeDesc}
       </span>
       <Image
         src='/book-nexus-logo.svg'
