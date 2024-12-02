015f6e050ad1cd59b448534d9f07cbf2ccbecbf9
Agustin Galante <agustin.galante.19@outlook.es>
2024-12-02 10:00:31 -0300

    using carousel at homepage

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,25 +1,51 @@
-import BookCard from '@/components/book/book-card';
 import Topbar from '@/components/topbar';
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+} from '@/components/ui/carousel';
+import BookCard from '@/components/book/book-card';
 import MOCK_BOOK from '@/lib/mockbook';
 
-export default async function Home() {
+export default function Home() {
   return (
     <main>
       <Topbar />
       <div className='container mx-auto p-4 space-y-8'>
         <section>
           <h3 className='font-semibold text-2xl'>My Readlist</h3>
-          <div className='flex gap-4 my-2'>
-            <BookCard book={MOCK_BOOK} mode='vertical' />
-            <BookCard book={MOCK_BOOK} mode='vertical' />
-          </div>
+          <Carousel
+            opts={{
+              align: 'start',
+              dragFree: true,
+            }}
+            className='w-full max-w-sm'
+          >
+            <CarouselContent className='-ml-16'>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem key={index} className='basis-1/3 pl-16'>
+                  <BookCard book={MOCK_BOOK} mode='vertical' />
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
         </section>
         <section>
-          <h3 className='font-semibold text-2xl my-2'>Readed</h3>
-          <div className='flex gap-4 overflow-y-auto'>
-            <BookCard book={MOCK_BOOK} />
-            <BookCard book={MOCK_BOOK} />
-          </div>
+          <h3 className='font-semibold text-2xl'>Readed</h3>
+          <Carousel
+            opts={{
+              align: 'start',
+            }}
+            className='w-full max-w-sm'
+          >
+            <CarouselContent className='-ml-8'>
+              {Array.from({ length: 5 }).map((_, index) => (
+                <CarouselItem key={index} className='pl-8'>
+                  <BookCard book={MOCK_BOOK} />
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+          </Carousel>
         </section>
       </div>
     </main>
