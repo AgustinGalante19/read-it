7165b658181d05b47c04f92e620f31387d991afa
Agustin Galante <agustin.galante.19@outlook.es>
2024-12-02 08:55:38 -0300

    Get book by id from google api

--- src/services/GoogleBooks.ts
+++ src/services/GoogleBooks.ts
@@ -1,9 +1,17 @@
-import { GoogleBooksResponse } from '@/types/Book';
+'use server';
+
+import { GoogleBookItem, GoogleBooksResponse } from '@/types/Book';
 
 const BASE_GOOGLE_API_URL = 'https://www.googleapis.com/books/v1';
 
 export async function getBooks(query: string): Promise<GoogleBooksResponse> {
   const request = await fetch(`${BASE_GOOGLE_API_URL}/volumes?q=${query}`);
+  const booksResponse = await request.json();
+  return booksResponse;
+}
+
+export async function getBook(id: string): Promise<GoogleBookItem> {
+  const request = await fetch(`${BASE_GOOGLE_API_URL}/volumes/${id}`);
   const booksResponse = await request.json();
   return booksResponse;
 }
