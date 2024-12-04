96b84d41b47f8ba22c22288e4eb2ea58165a780d
Agustin Galante <agustin.galante.19@outlook.es>
2024-12-04 10:24:33 -0300

    pwa implementation

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -6,7 +6,7 @@ import BottomBar from '@/components/bottombar';
 
 export const metadata: Metadata = {
   title: 'Read It',
-  description: '',
+  description: 'My personal to use as readlist',
 };
 
 const GeistFont = Geist({ subsets: ['latin'] });
@@ -18,6 +18,21 @@ export default function RootLayout({
 }>) {
   return (
     <html lang='en'>
+      <head>
+        <link
+          rel='icon'
+          type='image/png'
+          href='/favicon-96x96.png'
+          sizes='96x96'
+        />
+        <link rel='icon' type='image/svg+xml' href='/favicon.svg' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <link rel='manifest' href='/site.webmanifest' />
+      </head>
       <body className={`${GeistFont.className} antialiased`}>
         <Container>
           <div className='flex flex-col min-h-screen'>
---
+++ src/app/manifest.ts
@@ -0,0 +1,27 @@
+import type { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'Read It',
+    short_name: 'Read It',
+    description: 'My personal to use as readlist',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#ffffff',
+    theme_color: '#000000',
+    icons: [
+      {
+        src: '/web-app-manifest-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/web-app-manifest-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+    ],
+  };
+}
