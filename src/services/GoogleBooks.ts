0b76d126867386b425dbff210202d7e53697bc3c
Agustin Galante <agustin.galante.19@outlook.es>
2024-12-09 10:19:40 -0300

    caching google api request

--- src/services/GoogleBooks.ts
+++ src/services/GoogleBooks.ts
@@ -5,13 +5,17 @@ import { GoogleBookItem, GoogleBooksResponse } from '@/types/Book';
 const BASE_GOOGLE_API_URL = 'https://www.googleapis.com/books/v1';
 
 export async function getBooks(query: string): Promise<GoogleBooksResponse> {
-  const request = await fetch(`${BASE_GOOGLE_API_URL}/volumes?q=${query}`);
+  const request = await fetch(`${BASE_GOOGLE_API_URL}/volumes?q=${query}`, {
+    cache: 'force-cache',
+  });
   const booksResponse = await request.json();
   return booksResponse;
 }
 
 export async function getBook(id: string): Promise<GoogleBookItem> {
-  const request = await fetch(`${BASE_GOOGLE_API_URL}/volumes/${id}`);
+  const request = await fetch(`${BASE_GOOGLE_API_URL}/volumes/${id}`, {
+    cache: 'force-cache',
+  });
   const booksResponse = await request.json();
   return booksResponse;
 }
