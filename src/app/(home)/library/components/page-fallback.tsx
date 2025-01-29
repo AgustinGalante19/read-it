0d37b31a15c7f0f4557dc84d3611629959336a26
Agustin Galante <agustin.galante.19@outlook.es>
2025-01-29 09:41:03 -0300

    adding suspense & fallback to library page

---
+++ src/app/(home)/library/components/page-fallback.tsx
@@ -0,0 +1,46 @@
+import { Skeleton } from '@/components/ui/skeleton';
+
+function PageFallback() {
+  return (
+    <div>
+      <header className='w-full flex p-4 justify-between items-center container mx-auto'>
+        <span className='text-2xl font-bold text-white flex items-center gap-2 underline decoration-primary'>
+          <Skeleton className='w-[124px] h-[32px]' />
+        </span>
+        <Skeleton className='w-[24px] h-[24px]' />
+      </header>
+      <div className='flex items-center gap-4 px-4'>
+        <Skeleton className='w-[130px] h-[36px] rounded-full' />
+        <Skeleton className='w-[110px] h-[36px] rounded-full' />
+        <Skeleton className='w-[120px] h-[36px] rounded-full' />
+      </div>
+      {/* <section className='px-4 mt-2'>
+        <Label>
+          Title
+          <Input
+            placeholder='Search by title'
+            value={searchTerm}
+            onChange={onSearchTermChange}
+          />
+        </Label>
+      </section> */}
+      <section>
+        <div className='128x172 grid grid-cols-2 gap-2 py-4'>
+          {Array.from({ length: 6 }).map((_, i) => (
+            <div key={i} className='h-[272px] w-[128px] space-y-1 mx-auto'>
+              <Skeleton className='w-full h-[190px]' />
+              <Skeleton className='w-[128px] h-[22px]' />
+              <Skeleton className='w-[128px] h-[22px]' />
+              <div className='flex items-center gap-2'>
+                <Skeleton className='w-[128px] h-[16px]' />
+                <Skeleton className='w-[50px] h-[16px]' />
+              </div>
+            </div>
+          ))}
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default PageFallback;
--- src/app/(home)/library/page.tsx
+++ src/app/(home)/library/page.tsx
@@ -1,134 +1,15 @@
 'use client';
 
-import { ChangeEvent, useEffect, useState } from 'react';
-import { BookCheck, BookMarked, BookX, Library } from 'lucide-react';
-import filterBooksByStatus from '@/lib/filterBooksByStatus';
-import { getMyBooks } from '@/services/Library';
-import BookCard from '@/components/book/book-card';
-import { Input } from '@/components/ui/input';
-import { Label } from '@/components/ui/label';
-import { Skeleton } from '@/components/ui/skeleton';
-import StatusSelection from './components/status-selection';
-import { Book, BookStatus } from '@/types/Book';
-import Option from './types/Option';
-import { useSearchParams } from 'next/navigation';
+import { Suspense } from 'react';
+import LibraryContent from './components/library-content';
+import PageFallback from './components/page-fallback';
 
-const options: Option[] = [
-  { id: 3, value: 'all', label: 'Saved Books', icon: <BookMarked size={18} /> },
-  { id: 1, value: 'readed', label: 'Readed', icon: <BookCheck size={18} /> },
-  { id: 2, value: 'notReaded', label: 'Not Readed', icon: <BookX size={18} /> },
-];
-
-export default function LibraryPage() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [allBooks, setAllBooks] = useState<Book[]>([]);
-  const [booksList, setBooksList] = useState<Book[]>([]);
-  const [bookStatus, setBookStatus] = useState<Option>(options[0]);
-  const [searchTerm, setSearchTerm] = useState('');
-
-  const searchParams = useSearchParams();
-
-  const readStatus = searchParams.get('status');
-
-  useEffect(() => {
-    const getBooks = async () => {
-      try {
-        setIsLoading(true);
-        const currentReadStatus: BookStatus =
-          (readStatus as BookStatus) ?? 'all';
-        const { result: allBooksResponse } = await getMyBooks('all');
-        let booksResult = allBooksResponse;
-        if (readStatus) {
-          booksResult = filterBooksByStatus(
-            allBooksResponse,
-            currentReadStatus
-          );
-        }
-        setBooksList(booksResult);
-        setAllBooks(allBooksResponse);
-        setBookStatus(
-          options.find((el) => el.value === currentReadStatus) ?? options[0]
-        );
-      } catch (err) {
-        console.log(err);
-      } finally {
-        setIsLoading(false);
-      }
-    };
-    getBooks();
-  }, []);
-
-  const onSearchTermChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    setSearchTerm(value);
-    if (!value) {
-      const books = filterBooksByStatus(allBooks, bookStatus.value);
-      setBooksList(books);
-      return;
-    }
-    const newBooksList = booksList.filter((book) =>
-      book.title.toLowerCase().includes(value.toLowerCase())
-    );
-    setBooksList(newBooksList);
-  };
-
-  const handleChangeStatus = (newOption: Option) => {
-    setBookStatus(newOption);
-    const newBooks = filterBooksByStatus(allBooks, newOption.value);
-    setSearchTerm('');
-    setBooksList(newBooks);
-  };
-
+function LibraryPage() {
   return (
-    <div>
-      <header className='w-full flex p-4 justify-between items-center container mx-auto'>
-        <span className='text-2xl font-bold text-white flex items-center gap-2 underline decoration-primary'>
-          My Library
-        </span>
-        <Library className='text-primary' />
-      </header>
-      <StatusSelection
-        isLoading={isLoading}
-        bookStatus={bookStatus}
-        booksList={booksList}
-        handleChangeStatus={handleChangeStatus}
-        options={options}
-      />
-      <section className='px-4 mt-2'>
-        <Label>
-          Title
-          <Input
-            placeholder='Search by title'
-            value={searchTerm}
-            onChange={onSearchTermChange}
-          />
-        </Label>
-      </section>
-      <section>
-        {isLoading ? (
-          <div className='128x172 grid grid-cols-2 gap-2 py-4'>
-            {Array.from({ length: 6 }).map((_, i) => (
-              <div key={i} className='h-[272px] w-[128px] space-y-1 mx-auto'>
-                <Skeleton className='w-full h-[190px]' />
-                <Skeleton className='w-[128px] h-[22px]' />
-                <Skeleton className='w-[128px] h-[22px]' />
-                <div className='flex items-center gap-2'>
-                  <Skeleton className='w-[128px] h-[16px]' />
-                  <Skeleton className='w-[50px] h-[16px]' />
-                </div>
-              </div>
-            ))}
-          </div>
-        ) : (
-          <ul className='grid grid-cols-2 gap-2 py-4'>
-            {booksList.map((book) => (
-              <li key={book.id} className='mx-auto w-fit'>
-                <BookCard book={book} mode='vertical' />
-              </li>
-            ))}
-          </ul>
-        )}
-      </section>
-    </div>
+    <Suspense fallback={<PageFallback />}>
+      <LibraryContent />
+    </Suspense>
   );
 }
+
+export default LibraryPage;
