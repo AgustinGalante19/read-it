45b4c38e802cb8dd89689a2cb3b55718ff34723b
Agustin Galante <agustin.galante.19@outlook.es>
2025-03-12 12:45:16 -0300

    returning to the previous route organization

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,21 +1,54 @@
-'use client';
+import Topbar from '@/components/topbar';
+import { getMyBooks } from '@/services/Library';
+import BooksList from '@/components/book/books-list';
+import ShowAll from '@/components/book/show-all';
+import Link from 'next/link';
 
-import { Button } from '@/components/ui/button';
-import { signIn, useSession } from 'next-auth/react';
+export default async function Home() {
+  const readList = await getMyBooks('notReaded');
+  const readedBooks = await getMyBooks('readed');
+  const allBooks = await getMyBooks('all');
 
-function HomePage() {
-  const { data } = useSession();
-
-  console.log(data?.user);
   return (
-    <div>
-      {!data?.user ? (
-        <Button onClick={() => signIn('google')}>Login</Button>
-      ) : (
-        <span>{data.user.name}</span>
-      )}
-    </div>
+    <main>
+      <Topbar />
+      <div className='container mx-auto p-4 space-y-8'>
+        <section>
+          <ShowAll label='My Readlist' readStatus='notReaded' />
+          {readList.result.length > 0 ? (
+            <>
+              <BooksList
+                books={readList.result}
+                cardMode='vertical'
+                opts={{ dragFree: true }}
+                itemClassName='basis-1/3'
+              />
+            </>
+          ) : (
+            <div className='flex flex-col items-center mt-4 gap-2'>
+              <span className='text-gray-300'>
+                You don&apos;t have any book to read...
+              </span>
+              <Link href='/search' className='underline'>
+                Search one
+              </Link>
+            </div>
+          )}
+        </section>
+        <section>
+          <ShowAll label='Readed' readStatus='readed' />
+          <BooksList books={readedBooks.result} opts={{ dragFree: true }} />
+        </section>
+        <section>
+          <ShowAll label='All my Books' readStatus='all' />
+          <BooksList
+            books={allBooks.result}
+            cardMode='vertical'
+            opts={{ dragFree: true }}
+            itemClassName='basis-1/3'
+          />
+        </section>
+      </div>
+    </main>
   );
 }
-
-export default HomePage;
