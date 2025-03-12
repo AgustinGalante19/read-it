bdda7743281f85426aee42a245ee61b89f035f6b
Agustin Galante <agustin.galante.19@outlook.es>
2025-03-12 13:32:28 -0300

    sign in form finished

---
+++ src/app/sign-in/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from 'next';
+import { Geist } from 'next/font/google';
+import '@/app/globals.css';
+import Container from '@/components/ui/container';
+import Providers from '@/components/providers';
+
+export const metadata: Metadata = {
+  title: 'Read It',
+  description: 'My personal to use as readlist',
+};
+
+const GeistFont = Geist({ subsets: ['latin'] });
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang='en'>
+      <head>
+        <link rel='icon' type='image/svg+xml' href='/favicon.svg' />
+      </head>
+      <body className={`${GeistFont.className} antialiased`}>
+        <Container>
+          <Providers>
+            <div className='flex flex-col min-h-screen'>
+              <div className='flex-1 relative'>{children}</div>
+            </div>
+          </Providers>
+        </Container>
+      </body>
+    </html>
+  );
+}
