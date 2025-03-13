ce592957bcd23b8ed5e8ba243a1c3d16a2777125
Agustin Galante <agustin.galante.19@outlook.es>
2025-03-13 09:34:56 -0300

    create user if not exists in the database on logging in

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
+import { sql } from '@vercel/postgres';
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+
 const handler = NextAuth({
   providers: [
     GoogleProvider({
@@ -7,6 +9,26 @@ const handler = NextAuth({
       clientSecret: process.env.AUTH_GOOGLE_SECRET || '',
     }),
   ],
+  callbacks: {
+    signIn: async ({ user }) => {
+      try {
+        const { name, email, id: google_id } = user;
+
+        const userExists =
+          await sql`SELECT email from readit_users where email = ${email}`;
+
+        if (userExists.rows.length > 0) {
+          return true;
+        }
+
+        await sql`INSERT INTO readit_users (name, email, google_id) values(${name}, ${email}, ${google_id})`;
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
