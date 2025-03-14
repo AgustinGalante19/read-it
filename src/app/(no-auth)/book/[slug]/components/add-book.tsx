3c7849f992d18503c5dd154cb61f1a80487d1339
Agustin Galante <agustin.galante.19@outlook.es>
2025-03-14 09:17:28 -0300

    only add book if its authenticated

--- src/app/(no-auth)/book/[slug]/components/add-book.tsx
+++ src/app/(no-auth)/book/[slug]/components/add-book.tsx
@@ -4,10 +4,21 @@ import { Button } from '@/components/ui/button';
 import { addBook } from '@/services/Library';
 import { GoogleBookItem } from '@/types/Book';
 import { Plus } from 'lucide-react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
 import { toast } from 'sonner';
 
 function AddBook({ book }: { book: GoogleBookItem }) {
+  const { status: sessionStatus } = useSession();
+
+  const { push } = useRouter();
+
   const handleAddBook = async () => {
+    if (sessionStatus === 'unauthenticated') {
+      push('/sign-in');
+      return;
+    }
+
     const { result, status } = await addBook(book);
     if (!status) {
       return toast.error(result);
