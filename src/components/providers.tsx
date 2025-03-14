047424aa5c4b4007798ad2a61f311bdaf66f8eb6
Agustin Galante <agustin.galante.19@outlook.es>
2025-03-14 09:15:28 -0300

    duplicated layout in book/[slug] page

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,9 +2,15 @@
 
 import React from 'react';
 import { SessionProvider } from 'next-auth/react';
+import { Toaster } from './ui/sonner';
 
 function Providers({ children }: { children: React.ReactNode }) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return (
+    <SessionProvider>
+      <Toaster />
+      {children}
+    </SessionProvider>
+  );
 }
 
 export default Providers;
