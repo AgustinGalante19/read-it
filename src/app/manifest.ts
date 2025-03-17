c3a9021bb049fbe275f851c539b93507160249f2
Agustin Galante <agustin.galante.19@outlook.es>
2025-03-17 09:41:42 -0300

    assets organization

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -5,51 +5,23 @@ export default function manifest(): MetadataRoute.Manifest {
     name: 'Read-It',
     short_name: 'Read-It',
     description: 'The best app to make your readlist',
+    icons: [
+      {
+        src: '/web-app-manifest-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+      {
+        src: '/web-app-manifest-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+    ],
     start_url: '/',
     display: 'standalone',
     background_color: '#000000',
     theme_color: '#000000',
-    icons: [
-      {
-        src: '/favicon.ico',
-        sizes: '16x16',
-        type: 'image/ico',
-      },
-      {
-        src: '/mstile-70x70.png',
-        sizes: '70x670',
-        type: 'image/png',
-      },
-      {
-        src: '/mstile-144x144.png',
-        sizes: '144x144',
-        type: 'image/png',
-      },
-      {
-        src: '/mstile-150x150.png',
-        sizes: '150x150',
-        type: 'image/png',
-      },
-      {
-        src: '/mstile-310x150.png',
-        sizes: '310x150',
-        type: 'image/png',
-      },
-      {
-        src: '/mstile-310x310.png',
-        sizes: '310x310',
-        type: 'image/png',
-      },
-      {
-        src: '/android-chrome-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
-      },
-      {
-        src: '/android-chrome-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-      },
-    ],
   };
 }
