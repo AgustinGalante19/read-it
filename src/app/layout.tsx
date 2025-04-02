a802883d490e483a5e0e42351bd0ed8555f265a2
Agustin Galante <agustin.galante.19@outlook.es>
2025-04-02 18:44:50 -0300

    fix html syntax error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,22 +18,20 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <head>
-        <head>
-          <link
-            rel='icon'
-            type='image/png'
-            href='/favicon-96x96.png'
-            sizes='96x96'
-          />
-          <link rel='icon' type='image/svg+xml' href='/favicon.svg' />
-          <link rel='shortcut icon' href='/favicon.ico' />
-          <link
-            rel='apple-touch-icon'
-            sizes='180x180'
-            href='/apple-touch-icon.png'
-          />
-          <meta name='apple-mobile-web-app-title' content='Read-It' />
-        </head>
+        <link
+          rel='icon'
+          type='image/png'
+          href='/favicon-96x96.png'
+          sizes='96x96'
+        />
+        <link rel='icon' type='image/svg+xml' href='/favicon.svg' />
+        <link rel='shortcut icon' href='/favicon.ico' />
+        <link
+          rel='apple-touch-icon'
+          sizes='180x180'
+          href='/apple-touch-icon.png'
+        />
+        <meta name='apple-mobile-web-app-title' content='Read-It' />
       </head>
       <body className={`${GeistFont.className} antialiased`}>
         <Providers>{children}</Providers>
