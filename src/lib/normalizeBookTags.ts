dfe610ce907393b37b3e01a5b830bb784aa9c33a
Agustin Galante <agustin.galante.19@outlook.es>
2025-04-02 18:47:51 -0300

    prevent stats page crash if book hasn't tags

--- src/lib/normalizeBookTags.ts
+++ src/lib/normalizeBookTags.ts
@@ -1,8 +1,12 @@
 export default function normalizeBookTags(bookTags: { tags: string }[]) {
   const unrepeatedTags = new Set(
-    bookTags.flatMap(({ tags }) =>
-      tags.split('/').map((subTag: string) => subTag.trim())
-    )
+    bookTags.flatMap(({ tags }) => {
+      if (!tags) {
+        return '';
+      } else {
+        return tags.split('/').map((subTag: string) => subTag.trim());
+      }
+    })
   );
   return unrepeatedTags;
 }
