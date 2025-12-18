2d1a4e7c187911dbdf6e91998f202dd0ac944a9a
Agustin Galante <agustin.galante.19@outlook.es>
2025-12-18 20:48:14 -0300

    nextjs 16 migration

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,9 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ hostname: 'books.google.com' }],
   },
+  experimental: {
+    mcpServer: true,
+  },
 };
 
 export default nextConfig;
